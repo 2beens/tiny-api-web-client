54d0e4e02092089695b830f15e3c7b7240bcc635
serj.tubin <stubin87@gmail.com>
2022-09-27 19:41:51 +0200

    axios added; ping server and show resp

--- src/components/ServerStatus.vue
+++ src/components/ServerStatus.vue
@@ -22,6 +22,8 @@
 </template>
 
 <script>
+  import axios from 'axios'
+
   export default {
     name: 'ServerStatus',
   
@@ -31,8 +33,26 @@
 
     methods: {
       getStatus () {
-        console.log('todo')
+        const vm = this
+        axios
+          .get(process.env.VUE_APP_TINY_API_ENDPOINT + '/ping')
+          .then(response => {
+            if (response === null || response.data === null) {
+              console.error('ping server: received null response / data')
+              return
+            }
+            console.log('ping response', response.data)
+            vm.status = response.data.message
+          })
+          .catch(error => {
+            console.log(error)
+            vm.status = error.message
+          })
       },
+    },
+
+    mounted: function () {
+      this.getStatus()
     },
   }
   </script>
