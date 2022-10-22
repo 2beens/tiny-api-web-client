f8de7adff8200384de38cabeb0c60a90f2204c4b
serj.tubin <stubin87@gmail.com>
2022-10-22 12:57:03 +0200

    print stock deltas raw data

--- src/components/TinyStockChart.vue
+++ src/components/TinyStockChart.vue
@@ -1,48 +1,63 @@
 <template>
-  <v-card
-    v-if="stock" 
-    class="mx-auto text-center"
-    color="cyan"
-    dark
-  >
-    <v-card-text>
-      <v-sheet color="blue">
-        <v-sparkline
-          :labels="labels"
-          :value="value"
-          color="rgba(255, 255, 255, .7)"
-          height="100"
-          padding="24"
-          stroke-linecap="round"
-          smooth
+  <v-container>
+    <v-row>
+      <v-col>
+        <v-card
+          v-if="stock" 
+          class="mx-auto text-center"
+          color="cyan"
+          dark
         >
-          <template v-slot:label="item">
-            {{ item.value }} $
-          </template>
-        </v-sparkline>
-      </v-sheet>
-    </v-card-text>
+          <v-card-text>
+            <v-sheet color="blue">
+              <v-sparkline
+                :labels="labels"
+                :value="value"
+                color="rgba(255, 255, 255, .7)"
+                height="100"
+                padding="24"
+                stroke-linecap="round"
+                smooth
+              >
+                <template v-slot:label="item">
+                  {{ item.value }} $
+                </template>
+              </v-sparkline>
+            </v-sheet>
+          </v-card-text>
 
-    <v-card-text>
-      <div class="text-h3">
-        {{ stock.ticker }}
-      </div>
-      <div class="text-h4 font-weight-thin">
-        {{ stock.name }}
-      </div>
-    </v-card-text>
+          <v-card-text>
+            <div class="text-h3">
+              {{ stock.ticker }}
+            </div>
+            <div class="text-h4 font-weight-thin">
+              {{ stock.name }}
+            </div>
+          </v-card-text>
 
-    <v-divider></v-divider>
+          <v-divider></v-divider>
 
-    <v-card-actions class="justify-center">
-      <v-btn
-        block
-        text
-      >
-        Go to Report
-      </v-btn>
-    </v-card-actions>
-  </v-card>
+          <v-card-actions class="justify-center">
+            <v-btn
+              block
+              text
+              @click="printRawData"
+            >
+              Print Raw Data
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-textarea
+        clearable
+        clear-icon="mdi-close-circle"
+        label="Deltas Raw Data"
+        :value="deltasRawData"
+      ></v-textarea>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -54,6 +69,7 @@ export default {
     }
   },
   data: () => ({
+    deltasRawData: '',
     labels: [
     '12am',
     '3am',
@@ -75,5 +91,10 @@ export default {
       240,
     ],
   }),
+  methods: {
+    printRawData() {
+      this.deltasRawData = JSON.stringify(this.stock.deltas)
+    }
+  }
 }
 </script>
