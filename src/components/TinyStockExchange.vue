3c27e442fc5b67f5065597c7a2ae18e264d67522
serj.tubin <stubin87@gmail.com>
2022-10-24 08:53:58 +0200

    chart removed; added to exchange component

--- src/components/TinyStockExchange.vue
+++ src/components/TinyStockExchange.vue
@@ -3,10 +3,53 @@
     <TinyStocksList
       @update:selected="showChart"
     />
+
     <br/>
-    <TinyStockChart
-      :stock="selectedStock"
-    />
+
+    <v-row>
+      <v-col>
+        <v-card
+          v-if="selectedStock" 
+          class="mx-auto text-center"
+          color="cyan"
+          dark
+        >
+          <v-card-text>
+            <v-sheet color="blue">
+              <v-sparkline
+                :value="deltas"
+                color="rgba(255, 255, 255, .7)"
+                height="100"
+                padding="24"
+                stroke-linecap="round"
+                smooth
+              >
+                <template v-slot:label="item">
+                  {{ item.value }} $
+                </template>
+              </v-sparkline>
+            </v-sheet>
+          </v-card-text>
+
+          <v-card-text>
+            <div class="text-h3">
+              {{ selectedStock.ticker }}
+            </div>
+            <div class="text-h4 font-weight-thin">
+              {{ selectedStock.name }}
+            </div>
+          </v-card-text>
+
+          <v-divider></v-divider>
+
+          <v-card-actions class="justify-center">
+            <v-btn block text>
+              Add Delta
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
     <br/>
   </v-container>
 </template>
@@ -14,21 +57,21 @@
 <script>
 import axios from 'axios'
 import TinyStocksList from './TinyStocksList.vue'
-import TinyStockChart from './TinyStockChart.vue'
 
 export default {
   components: {
     TinyStocksList,
-    TinyStockChart
   },
 
   data: () => ({
-    selectedStock: null
+    selectedStock: null,
+    deltas: []
   }),
 
   methods: {
     showChart(stock) {
       this.selectedStock = stock
+      this.deltas = []
 
       const vm = this
       axios
@@ -44,8 +87,12 @@ export default {
             return
           }
 
-          const deltas = JSON.parse(response.data.message)
-          vm.selectedStock.deltas = deltas
+          // debugger; // eslint-disable-line no-debugger
+          JSON.parse(response.data.message).forEach(d => {
+            vm.deltas.push(d.delta)
+          })
+
+          console.log('gotten', vm.deltas.length, 'deltas for stock', vm.selectedStock.ticker)
         })
         .catch(error => {
           console.log(error)
